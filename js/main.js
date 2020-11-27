a92a891a70356c952fccb40f425411e2a87ac755
abdul2706 <abdulrehmankhan27061998@gmail.com>
2020-11-28 04:06:04 +0500

    finished at least for now

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 var TAG = "[main]";
 var clog = console.log;
-var total_sections = 0;
-var education_slide_id = 0;
-var total_slides = 3;
 
 $(function () {
     clog(TAG + "[starts]");
-    total_sections = $("section.slide").length;
-    clog(total_sections);
     $(".slider-button").click(function () {
-        clog("[slider-button][clicked]");
+        let total_slides = $(this).parent().children().length - 2;
+        let section_id = $(this).parent().parent().attr("id");
         let type = $(this).attr("type");
         let slide_item = $(this).siblings(".current-item");
-        clog(type);
-        current_slide_id = $(slide_item).attr("item-id");
+        current_slide_id = Number.parseInt($(slide_item).attr("item-id"));
         if (type == "left") {
             new_slide_id = (current_slide_id - 1) % total_slides;
         } else if (type == "right") {
@@ -22,9 +17,9 @@ $(function () {
         if (new_slide_id < 0) {
             new_slide_id += total_slides;
         }
-        clog(current_slide_id, new_slide_id);
         $(slide_item).removeClass("current-item");
-        $(`.slider-item[item-id=${new_slide_id}]`).addClass("current-item");
+        let selector = `#${section_id} .slider-item[item-id=${new_slide_id}]`;
+        $(selector).addClass("current-item");
     });
     clog(TAG + "[ends]");
 });
